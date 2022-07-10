068adb3f871a6355fcff420cc1031ee8ec07765f
isalzufari <isalzufari@gmail.com>
2022-07-10 21:08:17 +0700

    "Removed Account"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,22 +11,8 @@ const Navbar = ({ Link }) => {
             <Link to="/" className="m-2 link-dark text-decoration-none">Home</Link>
             <Link to="/store" className="m-2 link-dark text-decoration-none">Store</Link>
             <Link to="/balance" className="m-2 link-dark text-decoration-none">Balance</Link>
+            <Link className="dropdown-item" to="/add-product">Add Product</Link>
           </div>
-          <div className="p-2">
-            <a href="/#" className="link-dark"><i className="bi bi-cart"></i></a>
-          </div>
-          <div className="p-2 flex-shrink-0 dropdown">
-            <a href="/#" className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://dummyimage.com/50x50/000/ffffff.png" alt="mdo" width="32" height="32" className="rounded-circle" />
-            </a>
-            <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-              <li><Link className="dropdown-item" to="/add-product">Add Product</Link></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="/" onClick={() => { localStorage.removeItem("dataUser") }}>Sign out</a></li>
-            </ul>
-          </div>
-          <p className="m-2">isal</p>
-          <p className="m-2"><a className="text-dark text-decoration-none" href="/login" style={{ cursor: "pointer" }}>Login</a></p>
         </div>
       </div>
     </header>
